860ae3c3583ff65994c354ac6c0031d1cc3722b9
khaan <nguyenkhaan2006@gmail.com>
2025-09-08 02:01:25 +0700

    them_credential

--- src/Aixos/api.js
+++ src/Aixos/api.js
@@ -1,5 +1,6 @@
 import axios from "axios"; 
 const api = axios.create({
-    baseURL: 'https://blogging-website-be.onrender.com'
+    baseURL: 'https://blogging-website-be.onrender.com', 
+    withCredentials: true
 })
 export default api
